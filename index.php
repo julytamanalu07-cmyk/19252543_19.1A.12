1b220d619d83b88754e7bf6ea035c87c25653d27
julytamanalu07-cmyk <julytamanalu07@gmail.com>
2026-05-05 22:00:33 +0700

    ini adalah tugas saya

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>kuis</title>
+</head>
+<body>
+ <!--Inputan nim, kelas, mata kuliah, dan jurusan  -->
+ <form action="">
+
+ <label for="">NIM:</label><input type="number"> <br>
+ <label for="">Kelas</label><input type="text"> <br>
+ <label for="">Mata Kuliah</label><input type="text"> <br>
+ <label for="">jurusan</label><input type="text">  
+ </form>
+
+</body>
+</html>
